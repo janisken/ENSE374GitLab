d2fac73ea4a133d5ca03d3232447e63e5499bc72
Noah Janiskevich <noah.janiskevich@gmail.com>
2015-10-26 20:18:30 -0600

    first phase

---
+++ ListElement.java
@@ -0,0 +1,43 @@
+public class ListElement{
+	
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement()
+	{
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
+	}
+	
+	public void setNext(ListElement next)
+	{
+		this.next = next;
+	}
+	
+	public ListElement getNext()
+	{
+		return this.next;
+	}
+	
+	public void setPrevious(ListElement previous)
+	{
+		this.previous = previous;
+	}
+	
+	public ListElement getPrevious()
+	{
+		return this.previous;
+	}
+	
+	public void setData(int data)
+	{
+		this.data = data;
+	}
+	
+	public int getData()
+	{
+		return this.data;
+	}
+}
