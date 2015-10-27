6ac2a7629c71369f8bc0c683ba7e043e7bde7acd
Noah Janiskevich <noah.janiskevich@gmail.com>
2015-10-26 22:04:01 -0600

    saving main

--- ListElementMain.java
+++ ListElementMain.java
@@ -1,2 +1,29 @@
 import java.io.IOException;
 import java.util.*;
+
+public class ListElementMain{
+	
+	public static void main (String[] args)
+	{
+		ListElement le = new ListElement();
+		List l = new List();
+		le.setData(5);
+		l.addElement(le);
+		le.setData(4);
+		l.addElement(le);
+		le.setData(3);
+		l.addElement(le);
+		le.setData(2);
+		l.addElement(le);
+		le.setData(1);
+		l.addElement(le);
+		
+		l.printLinkedListHead();
+		
+		ListElement x = l.getElement(4);
+		System.out.println(x.getData());
+		
+		l.deleteElement(4);
+		l.printLinkedListHead();
+	}
+}
